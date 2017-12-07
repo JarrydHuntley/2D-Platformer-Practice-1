ba1c3d1b9775464fb0258afd316282c764175e78
ZacFierce <magmaspire@gmail.com>
2017-12-07 15:27:33 -0500

    Character Animation
    - Imported walk and enemy sprites.
    - Created standing and walking animation for player character.
    - Updated player character controls to feel tighter.
    - Doubled gravity for better looking jumps.

--- Assets/Gameplay/PlayerCharacterScript.cs
+++ Assets/Gameplay/PlayerCharacterScript.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class PlayerCharacterScript : MonoBehaviour
 {
     [Header("Design Values")]
-    public float moveSpeed = 10f;
+    public float moveAccel = 1f;
+    public float moveDecel = 2f;
+    public float moveFriction = 0.9f;
+    public float maxMoveSpeed = 10f;
     public float jumpForce = 10f;
     public float jumpSpinTorque = 5f;
     [Header("References")]
     public PauseMenuScript pauseMenu;
 
     new Rigidbody2D rigidbody;
+    Animator animator;
     GroundedSense groundedSense;
     bool jumpInput;
 
     void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         groundedSense = GetComponent<GroundedSense>();
     }
 
@@ -25,19 +30,51 @@ public class PlayerCharacterScript : MonoBehaviour
     {
         if (Input.GetButtonDown("Pause"))
             pauseMenu.Pause();
-        
+
+        if (Input.GetButtonUp("Jump"))
+            jumpInput = false;
         if (Input.GetButtonDown("Jump"))
             jumpInput = true;
     }
 
     void FixedUpdate()
     {
-        rigidbody.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, rigidbody.velocity.y);
+        // Moving
+        float moveSpeed = rigidbody.velocity.x;
+        if (Input.GetAxisRaw("Horizontal") > 0) // If attempting to move right
+        {
+            animator.SetBool("Walking", true);
+            if (moveSpeed >= 0) // If already moving to the right
+                moveSpeed += Input.GetAxisRaw("Horizontal") * moveAccel; // Accelerate
+            else
+                moveSpeed += Input.GetAxisRaw("Horizontal") * moveDecel; // Decelerate
+            if (moveSpeed > maxMoveSpeed) // Don't move faster than max move speed
+                moveSpeed = maxMoveSpeed;
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0) // If attempting to move left
+        {
+            animator.SetBool("Walking", true);
+            if (moveSpeed <= 0) // If already moving to the left
+                moveSpeed += Input.GetAxisRaw("Horizontal") * moveAccel; // Accelerate
+            else
+                moveSpeed += Input.GetAxisRaw("Horizontal") * moveDecel; // Decelerate
+            if (moveSpeed < -maxMoveSpeed) // Don't move faster than max move speed
+                moveSpeed = -maxMoveSpeed;
+        }
+        else // Slow to a stop
+        {
+            animator.SetBool("Walking", false);
+            moveSpeed *= moveFriction;
+        }
 
+        // Set new movement speed
+        rigidbody.velocity = new Vector2(moveSpeed, rigidbody.velocity.y);
+
+        // Jump if attempting to jump while on the ground
         if (jumpInput && groundedSense.IsGrounded)
         {
             rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            rigidbody.AddTorque(Random.Range(-jumpSpinTorque, jumpSpinTorque));
+            rigidbody.AddTorque(Random.Range(-jumpSpinTorque, jumpSpinTorque)); // Add some spin
             jumpInput = false;
         }
     }
